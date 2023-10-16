923df91ef3b056b58fe9d9793b36bbf4e8acf210
Gerald Walker <gerald.e.walker05@gmail.com>
2023-10-16 14:18:13 -0500

    Contact work

--- src/Contacts.java
+++ src/Contacts.java
@@ -1,4 +1,3 @@
-package src;
 
 import java.io.IOException;
 import java.nio.file.Files;
@@ -7,13 +6,16 @@ import java.nio.file.Paths;
 import java.nio.file.StandardOpenOption;
 import java.util.Arrays;
 import java.util.List;
+import java.util.Scanner;
 
 public class Contacts {
 
     public static void main(String[] args) throws IOException {
 
-        String directory = "contacts";
-        String filename = "src.contacts.txt";
+        Scanner scanner = new Scanner(System.in);
+
+        String directory = "src";
+        String filename = "contacts.txt";
 
         Path dataDirectory = Paths.get(directory);
         Path dataFile = Paths.get(directory, filename);
@@ -35,32 +37,57 @@ public class Contacts {
         Files.write(dataFile, myData, StandardOpenOption.APPEND);
 
 
-        Files.write(
-                Paths.get("contacts", "src.contacts.txt"),
-                Arrays.asList("sad : 8"), // list with one item
-                StandardOpenOption.APPEND
-        );
+//        Files.write(
+//                Paths.get("src", "contacts.txt"),
+//                Arrays.asList("sad : 8"), // list with one item
+//                StandardOpenOption.APPEND
+//        );
 
         List<String> fileInfo = Files.readAllLines(dataFile);
 
-        for (String oneLine : fileInfo) {
-            //When using bar character \\ is needed to escape its normal function
-            String[] data = oneLine.split(" : ");
-            System.out.println(data[0]);
-            System.out.println(data[1]);
-//            System.out.println(data);
+//        for (String oneLine : fileInfo) {
+//            //When using bar character \\ is needed to escape its normal function
+//            String[] data = oneLine.split(" : ");
+//            System.out.println(data[0]);
+//            System.out.println(data[1]);
+////            System.out.println(data);
+//
+//        }
 
-            System.out.println("Hello, welcome to the phone company. Please select a option below.");
+        System.out.println("Hello, welcome to the phone company. Please select a option below.");
 
-            System.out.println("1. View src.contacts.\n" +
-                    "2. Add a new contact.\n" +
-                    "3. Search a contact by name.\n" +
-                    "4. Delete an existing contact.\n" +
-                    "5. Exit.\n" +
-                    "Enter an option (1, 2, 3, 4 or 5):\n");
+        System.out.println(
+                "1. View src.contacts.\n" +
+                "2. Add a new contact.\n" +
+                "3. Search a contact by name.\n" +
+                "4. Delete an existing contact.\n" +
+                "5. Exit.\n" +
+                "Enter an option (1, 2, 3, 4 or 5):\n");
+
+        String input1 = scanner.nextLine();
+
+        if ( input1.equals("1")) {
+            for (String oneLine : fileInfo) {
+                //When using bar character \\ is needed to escape its normal function
+                String[] data = oneLine.split(" : ");
+                System.out.println(data[0]);
+                System.out.println(data[1]);
+                ;
+            }
+        } else if (input1.equals("2")){
+            System.out.println("What is thier name and number?");
+            input1 = scanner.nextLine();
+
+            Files.write(
+                    Paths.get("src", "contacts.txt"),
+                    Arrays.asList(input1), // list with one item
+                    StandardOpenOption.APPEND
+            );
+
+        };
 
 
-        }
+        };
 
     }
 
